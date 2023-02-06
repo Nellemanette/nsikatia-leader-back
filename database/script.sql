bb5c335cae5bcc01468b370412ecbe1b8881b6f1
Brunelle <bmalandila@yahoo.fr>
2023-02-06 19:36:18 +0100

    Changed database scheme

--- database/script.sql
+++ database/script.sql
@@ -3,10 +3,14 @@ CREATE EXTENSION pgcrypto;
 
 DROP TABLE IF EXISTS Personne CASCADE;
 DROP TABLE IF EXISTS Fiche CASCADE;
+DROP TABLE IF EXISTS Pratique CASCADE;
 DROP TABLE IF EXISTS Info CASCADE;
 DROP TABLE IF EXISTS Disponibilite CASCADE;
 DROP TABLE IF EXISTS Reservation CASCADE;
 DROP TABLE IF EXISTS Cours CASCADE; 
+DROP TABLE IF EXISTS Identite CASCADE; 
+DROP TABLE IF EXISTS Compte CASCADE; 
+
 
 DROP TYPE IF EXISTS fonction CASCADE;
 DROP TYPE IF EXISTS pub CASCADE;
@@ -22,6 +26,9 @@ DROP SEQUENCE IF EXISTS disponibilite_id_seq;
 DROP SEQUENCE IF EXISTS cours_id_seq;
 DROP SEQUENCE IF EXISTS reservation_id_seq;
 DROP SEQUENCE IF EXISTS personne_id_seq;
+DROP SEQUENCE IF EXISTS identite_id_seq;
+DROP SEQUENCE IF EXISTS compte_id_seq;
+DROP SEQUENCE IF EXISTS pratique_id_seq;
 
 CREATE TYPE fonction AS ENUM ('student', 'teacher');
 CREATE TYPE pub AS ENUM ('bouche a oreille', 'reseaux sociaux', 'site web');
@@ -39,19 +46,19 @@ CREATE TABLE Identite(
    age SMALLINT NOT NULL,
    telephone CHAR(11) NOT NULL,
    ville VARCHAR(20) NOT NULL,
+   PRIMARY KEY(id)
 );
 
 CREATE TABLE Compte(
     id SMALLSERIAL,
     email VARCHAR(80) NOT NULL,
     mot_de_passe VARCHAR(80) NOT NULL,
+    PRIMARY KEY(id)
+
 );
 
-CREATE TABLE Fiche(
+CREATE TABLE Pratique(
     id SMALLSERIAL,
-    inscrit BOOLEAN NOT NULL,
-    code BOOLEAN NOT NULL,
-    conduite BOOLEAN NOT NULL,
     nbr_heures heures,
     demarrage_arret niveau,
     vitesse niveau,
@@ -65,24 +72,33 @@ CREATE TABLE Fiche(
     manoeuvre niveau,
     PRIMARY KEY(id)
 );
+CREATE TABLE Fiche(
+    id SMALLSERIAL,
+    inscrit BOOLEAN NOT NULL,
+    code BOOLEAN NOT NULL,
+    conduite BOOLEAN NOT NULL,
+    pratique_id SMALLINT,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_pratique FOREIGN KEY(pratique_id) REFERENCES Pratique(id) ON DELETE CASCADE
+);
 
 CREATE TABLE Info(
     id SMALLSERIAL,
     prospect pub NOT NULL,
     fiche_id SMALLINT NOT NULL,
     PRIMARY KEY(id),
-    CONSTRAINT fk_fiche FOREIGN KEY(fiche_id) REFERENCES Fiche(id)
+    CONSTRAINT fk_fiche FOREIGN KEY(fiche_id) REFERENCES Fiche(id) ON DELETE CASCADE
 );
 
 CREATE TABLE Personne(
    id SMALLSERIAL,
-   identite_id SMALLINT NOT NULL,
-   compte_id SMALLINT NOT NULL,
-   info_id SMALLINT NOT NULL,
+   identite_id SMALLINT,
+   compte_id SMALLINT,
+   info_id SMALLINT,
    PRIMARY KEY(id),
-   CONSTRAINT fk_identite FOREIGN KEY(identite_id) REFERENCES Identite(id),
-   CONSTRAINT fk_compte FOREIGN KEY(compte_id) REFERENCES Compte(id),
-   CONSTRAINT fk_info FOREIGN KEY(info_id) REFERENCES Info(id)
+   CONSTRAINT fk_identite FOREIGN KEY(identite_id) REFERENCES Identite(id) ON DELETE CASCADE,
+   CONSTRAINT fk_compte FOREIGN KEY(compte_id) REFERENCES Compte(id) ON DELETE CASCADE,
+   CONSTRAINT fk_info FOREIGN KEY(info_id) REFERENCES Info(id) ON DELETE CASCADE
 );
 
 CREATE TABLE Disponibilite(
@@ -110,29 +126,48 @@ CREATE TABLE Reservation(
     cours_id SMALLINT NOT NULL,
     eleve_id SMALLINT NOT NULL,
     PRIMARY KEY(id),
-    CONSTRAINT fk_eleve FOREIGN KEY(eleve_id) REFERENCES Personne(id),
+    CONSTRAINT fk_eleve FOREIGN KEY(eleve_id) REFERENCES Personne(id) ON DELETE CASCADE,
     CONSTRAINT fk_cours FOREIGN KEY(cours_id) REFERENCES Cours(id)
 );
 
 GRANT ALL PRIVILEGES ON Fiche TO n_sikatia_leader_bdd__user_c0;
 GRANT ALL PRIVILEGES ON Disponibilite TO n_sikatia_leader_bdd__user_c0;
+GRANT ALL PRIVILEGES ON Pratique TO n_sikatia_leader_bdd__user_c0;
 GRANT USAGE, SELECT ON SEQUENCE fiche_id_seq TO n_sikatia_leader_bdd__user_c0;
 GRANT USAGE, SELECT ON SEQUENCE disponibilite_id_seq TO n_sikatia_leader_bdd__user_c0;
+GRANT USAGE, SELECT ON SEQUENCE pratique_id_seq TO n_sikatia_leader_bdd__user_c0;
 
 GRANT ALL PRIVILEGES ON Info TO n_sikatia_leader_bdd__user_c1;
 GRANT ALL PRIVILEGES ON Cours TO n_sikatia_leader_bdd__user_c1;
+GRANT ALL PRIVILEGES ON Personne TO n_sikatia_leader_bdd__user_c1;
+GRANT ALL PRIVILEGES ON Identite TO n_sikatia_leader_bdd__user_c1;
+GRANT ALL PRIVILEGES ON Compte TO n_sikatia_leader_bdd__user_c1;
+GRANT ALL PRIVILEGES ON Fiche TO n_sikatia_leader_bdd__user_c1;
+GRANT ALL PRIVILEGES ON Pratique TO n_sikatia_leader_bdd__user_c1;
+GRANT ALL PRIVILEGES ON Disponibilite TO n_sikatia_leader_bdd__user_c1;
+GRANT ALL PRIVILEGES ON Cours TO n_sikatia_leader_bdd__user_c1;
 GRANT USAGE, SELECT ON SEQUENCE info_id_seq TO n_sikatia_leader_bdd__user_c1;
+GRANT USAGE, SELECT ON SEQUENCE cours_id_seq TO n_sikatia_leader_bdd__user_c1;
+GRANT USAGE, SELECT ON SEQUENCE personne_id_seq TO n_sikatia_leader_bdd__user_c1;
+GRANT USAGE, SELECT ON SEQUENCE identite_id_seq TO n_sikatia_leader_bdd__user_c1;
+GRANT USAGE, SELECT ON SEQUENCE compte_id_seq TO n_sikatia_leader_bdd__user_c1;
+GRANT USAGE, SELECT ON SEQUENCE fiche_id_seq TO n_sikatia_leader_bdd__user_c1;
+GRANT USAGE, SELECT ON SEQUENCE pratique_id_seq TO n_sikatia_leader_bdd__user_c1;
+GRANT USAGE, SELECT ON SEQUENCE disponibilite_id_seq TO n_sikatia_leader_bdd__user_c1;
 GRANT USAGE, SELECT ON SEQUENCE cours_id_seq TO n_sikatia_leader_bdd__user_c1;
 
 GRANT ALL PRIVILEGES ON Reservation TO n_sikatia_leader_bdd__user_c2;
+GRANT ALL PRIVILEGES ON Identite TO n_sikatia_leader_bdd__user_c2;
 GRANT USAGE, SELECT ON SEQUENCE reservation_id_seq TO n_sikatia_leader_bdd__user_c2;
+GRANT USAGE, SELECT ON SEQUENCE identite_id_seq TO n_sikatia_leader_bdd__user_c2;
 
-GRANT ALL PRIVILEGES ON Personne TO n_sikatia_leader_bdd__user_c3;
-GRANT USAGE, SELECT ON SEQUENCE personne_id_seq TO n_sikatia_leader_bdd__user_c3;
+GRANT ALL PRIVILEGES ON Compte TO n_sikatia_leader_bdd__user_c3;
+GRANT USAGE, SELECT ON SEQUENCE compte_id_seq TO n_sikatia_leader_bdd__user_c3;
 
 --
 --C0: Fiche Disponibilite
---C1: Info Cours
---C2: Reservation
---C3: Personne
+--C1: Info Cours Personne
+--C2: Reservation Identite
+--C3: Compte
 
+-- Revoir les parents et tout (one to one relashionship et la localisation des lcé étrangères)
